07db9172992f1f92ec0e42787eb7b8b55d8e1176
Eric-ZC-code <Zhang_Chen18@outlook.com>
2023-12-29 23:47:59 +0800

    file_upload module added

--- src/main/java/com/itheima/controller/UploadController.java
+++ src/main/java/com/itheima/controller/UploadController.java
@@ -1,7 +1,9 @@
 package com.itheima.controller;
 
 import com.itheima.pojo.Result;
+import com.itheima.utils.AliOSSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,17 +15,30 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class UploadController {
+
+    @Autowired
+    AliOSSUtils aliOSSUtils;
+//    @PostMapping("/upload")
+//    public Result upload(String username, Integer age, MultipartFile image) throws IOException {
+//        log.info("文件上传：{}, {}, {}", username, age, image);
+//
+//        //构造唯一文件名-uuid
+//        String originalFilename = image.getOriginalFilename();
+//        String newFilename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
+//
+//
+//        //将文件存储在本地
+//        image.transferTo(new File("D:\\图片\\Saved Pictures\\"+newFilename));
+//        return Result.success();
+//    }
+
     @PostMapping("/upload")
-    public Result upload(String username, Integer age, MultipartFile image) throws IOException {
-        log.info("文件上传：{}, {}, {}", username, age, image);
+    public Result upload(MultipartFile image) throws Exception {
+        log.info("文件上传，文件名：{}", image.getOriginalFilename());
+        //调用阿里云OSS工具类进行文件上传
+        String url = aliOSSUtils.upload(image);
+        log.info("文件上传完成，文件访问的url：{}", url);
 
-        //构造唯一文件名-uuid
-        String originalFilename = image.getOriginalFilename();
-        String newFilename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
-
-
-        //将文件存储在本地
-        image.transferTo(new File("D:\\图片\\Saved Pictures\\"+newFilename));
-        return Result.success();
+        return  Result.success(url);
     }
 }
