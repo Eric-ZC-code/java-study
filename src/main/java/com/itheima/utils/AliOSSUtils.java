38915796093a78d9acf74c5bcb8ea275108a90ff
Eric-ZC-code <Zhang_Chen18@outlook.com>
2023-12-30 13:02:24 +0800

    file_upload module modified

--- src/main/java/com/itheima/utils/AliOSSUtils.java
+++ src/main/java/com/itheima/utils/AliOSSUtils.java
@@ -2,6 +2,7 @@ package com.itheima.utils;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
@@ -15,25 +16,25 @@ public class AliOSSUtils {
 
     // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
     String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-    // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    String accessKeyId = "LTAI5tM6N5vy1XAWKWL6ptSC";
-    String accessKeySecret = "SNw9I6cxd4RZtkjjIeaEyFFHzFFyE8";
     // 填写Bucket名称，例如examplebucket。
     String bucketName = "ql-web-framework01";
 
     /**
      * 实现上传图片到OSS
      */
-    public String upload(MultipartFile file) throws IOException {
+    public String upload(MultipartFile file) throws Exception {
         // 获取上传的文件的输入流
         InputStream inputStream = file.getInputStream();
+
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
 
         // 避免文件覆盖
         String originalFilename = file.getOriginalFilename();
         String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
 
         //上传文件到 OSS
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
         ossClient.putObject(bucketName, fileName, inputStream);
 
         //文件访问路径
